58c3d2fa980afc11d4e595294cd6d0e6bb06a4ad
Ayushi Vineet Sharan <ayushi.v.sharan@gmail.com>
2023-05-22 17:07:43 +0530

    establishing connection to the database

---
+++ go.mod
@@ -0,0 +1,5 @@
+module students_db_postgreSQL
+
+go 1.20
+
+require github.com/lib/pq v1.10.9
--- main.go
+++ main.go
@@ -1,9 +1,30 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
+	"log"
+
+	_ "github.com/lib/pq"
 )
 
 func main() {
-	fmt.Println("Hello!")
+	// establishing connection to the database
+	fmt.Println("Connecting to db.... ")
+	connStr := "user=postgres dbname=testing password=''sslmode=disable"
+	db, err := sql.Open("postgres", connStr)
+	CheckError(err)
+	defer db.Close()
+
+	if err := db.Ping(); err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("\nSuccessfully connected to the database!")
+}
+
+func CheckError(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
 }
